3133a63156e8c32a4e8eafc661c5b03fe90b4bf3
lorini <lorini@eurecom.fr>
2021-11-14 13:58:29 +0000

    Fixed typo that broke elastic

--- application.py
+++ application.py
@@ -2,17 +2,17 @@ from flask import Flask
 from flask import jsonify
 import numpy as np
 
-applicaition = Flask(__name__)
+application = Flask(__name__)
 
-@applicaition.route('/')
+@application.route('/')
 def hello():
     return 'Hello Giulia\n'
     
-@applicaition.route('/random/<n>')
+@application.route('/random/<n>')
 def randomvalues(n):
     values = np.random.randint(0,10,int(n))
     result = {'values' : values.tolist()}
     return jsonify(result)
     
 if __name__ == '__main__':
-    applicaition.run(debug=True)
+    application.run(debug=True)
